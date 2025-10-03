220a22266b70cad3ae25e2146928cac7517c01b9
TamasMucsi <mucsi.tamas-2023@keri.mako.hu>
2025-10-03 10:20:36 +0200

    asddas

---
+++ js/script.js
@@ -0,0 +1,14 @@
+fetch("navbar.html")
+.then(response => response.text())
+.then(data => {
+document.getElementById("navbar-ref").innerHTML = data;
+})
+    .catch(error => console.error("Loading failed!", error));
+
+
+fetch("footer.html")
+    .then(response => response.text())
+    .then(data => {
+        document.querySelector(".footer").innerHTML = data;
+    })
+    .catch(error => console.error("Loading failed", error));
