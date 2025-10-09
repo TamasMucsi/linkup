75f354356283f87b2e96f055ce84a24977704a01
TamasMucsi <mucsi.tamas-2023@keri.mako.hu>
2025-10-09 11:26:59 +0200

    ssfgrrg

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-let nvar = document.getElementById("navbar-ref")
+let nvar = document.getElementById("navbar-ref");
 if (nvar) {
     fetch("navbar.html")
     .then(response => response.text())
@@ -7,8 +7,11 @@ if (nvar) {
     })
     .catch(error => console.error("Loading failed! :(", error));
 }
+// let nvbar = document.getElementById("navbar");
+// if (nvbar)
 else{
-    fetch("navbar.html")
+
+    fetch("navbar_mainpage.html")
     .then(response => response.text())
     .then(data => {
     document.getElementById("navbar").innerHTML = data;
