def961485b8d1a995e5185db73c55b56e0c0e3b3
Tóth Dániel <toth.daniel-2023@keri.mako.hu>
2025-10-09 11:15:57 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,12 +1,11 @@
-let nvar = document.getElementById("navbar-ref")
-if(nvar){
 fetch("navbar.html")
 .then(response => response.text())
 .then(data => {
 document.getElementById("navbar-ref").innerHTML = data;
 })
-    .catch(error => console.error("Loading failed!", error));
-}
+.catch(error => console.error("Loading failed! :(", error));
+
+
 
 fetch("footer.html")
     .then(response => response.text())
