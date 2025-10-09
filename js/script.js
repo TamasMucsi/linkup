18411a1cabd79a91413b5421189e4d2d2ab4f70f
TamasMucsi <mucsi.tamas-2023@keri.mako.hu>
2025-10-09 11:03:09 +0200

    sad

--- js/script.js
+++ js/script.js
@@ -1,10 +1,12 @@
+let nvar = document.getElementById("navbar-ref")
+if(nvar){
 fetch("navbar.html")
 .then(response => response.text())
 .then(data => {
 document.getElementById("navbar-ref").innerHTML = data;
 })
     .catch(error => console.error("Loading failed!", error));
-
+}
 
 fetch("footer.html")
     .then(response => response.text())
